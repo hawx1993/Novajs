d918ea0f446db27c65e60d3322e1fcf28d3e3a8e
trigkit4 <trigkit@163.com>
2015-04-20 20:33:08 +0800

    Update Nova.js
    增加了对cookie的操作

--- Nova.js
+++ Nova.js
@@ -344,7 +344,38 @@
             }
             return this;
         }
+        //创建cookie
+        setCookie : function(name, value, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+        if (expires instanceof Date) {
+            cookieText += '; expires=' + expires;
+        }
+        if (path) {
+            cookieText += '; expires=' + expires;
+        }
+        if (domain) {
+            cookieText += '; domain=' + domain;
+        }
+        if (secure) {
+            cookieText += '; secure';
+        }
+        document.cookie = cookieText;
+    }
 
+     //获取cookie
+      getCookie : function(name) {
+        var cookieName = encodeURIComponent(name) + '=';
+        var cookieStart = document.cookie.indexOf(cookieName);
+        var cookieValue = null;
+        if (cookieStart > -1) {
+            var cookieEnd = document.cookie.indexOf(';', cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+        }
+        return cookieValue;
+       }
 
     };
 
